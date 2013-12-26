605b6b2720bc7523b5a5738016faba2e2c1c1801
AtomskJD <AtomskJD@gmail.com>
2013-12-26 17:37:49 +0600

    Новая версия упаковщика на подходе
    Теперь выбор режима упаковки

--- pack.sh
+++ pack.sh
@@ -1,40 +1,40 @@
 #!/bin/bash
+# @author 	AtomskJD aka mintru9
+# version	0.10
 
-if [ $1 ]; 
-	then USER=$1
-else 
-	echo -e "set DB USER: \c"
-	read USER
-fi
-if [ $2 ]; 
-	then PASS=$2
-else 
-	echo -e "set PASSWORD: \c"
-	read PASS
-fi
-if [ $3 ]; 
-	then DB=$3
-else 
-	echo -e "set DB NAME: \c"
-	read DB
-fi
+settingsDir=sites/default/settings.php
 
-rm -fv dump.sql dump.tar.bz2
+USER=$(grep "^ *'username' => .*',$" $settingsDir | cut -d "'" -f 4)
+PASS=$(grep "^ *'password' => .*',$" $settingsDir | cut -d "'" -f 4)
+DB=$(grep "^ *'database' => .*',$" $settingsDir | cut -d "'" -f 4)
+
+printf "Choose backup type:\n[ F ] Full backup with DB & FileS\n[ DB ] for db backup\n[ FS ] for files backup"
+read backupType
+
+rm dump.sql dump.tar.bz2
 echo "CLEARING old dumps"
+	
+if [[ $backupType = @(DB | F) ]]; then
+	echo "----------------------------------------------------------------------"
+	echo -e "PACKING DB\c"
 
-echo -e "PACKING DB\c"
-if mysqldump -u$USER -p$PASS $DB > dump.sql ; then
-	echo -e "  \t\tSUCCESS"
-else
-	echo " \t\tFAIL MYSQLDUMP"
-	exit
+	if mysqldump -u$USER -p$PASS $DB > dump.sql ; then
+		echo -e "  \t\t[COMPLETE]"
+	else
+		echo "\n\t\tMYSQLDUMP [FAIL]"
+		exit
+	fi
 fi
 
-echo -e "PACKING FILES\c"
-if tar -cjf dump.tar.bz2 * .htaccess ; then
-	echo -e "  \t\tDUMP.tar.bz2 COMPLETE"
-else
-	echo " \t\tTAR FAIL"
-	exit
+if [[ $backupType = @(FS | F) ]]; then
+	echo "----------------------------------------------------------------------"
+	echo -e "PACKING FILES\c"
+	if tar -cjf dump.tar.bz2 * .htaccess ; then
+		echo -e "  \t\t[COMPLETE]"
+	else
+		echo " \t\tTAR [FAIL]"
+		exit
+	fi
 fi
-echo -e "go WGET it \t\tALLONS-Y!"
+
+echo -e "go WGET it \t\t [ALL DONE]"
