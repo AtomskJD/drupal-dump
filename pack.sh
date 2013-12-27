11d18bec757427f9c6ae1a84da87edd3ab91c58d
AtomskJD <AtomskJD@gmail.com>
2013-12-27 11:06:19 +0600

    Наведение красивостей
    нужен "тихий режим" для крона

--- pack.sh
+++ pack.sh
@@ -4,30 +4,34 @@
 
 settingsDir=sites/default/settings.php
 spaceUsed=$(du -sh | cut -f 1)
-
+echo "----------------------------------------------------------------------"
 printf "PRE PACKING INFO\nTotal site disk usage\t$spaceUsed\n"
 if (( $( ls -l | grep 'bz2$\|sql$' | wc -l) )) ; then
-	printf "[WARNING!] old backup detected\nCLEAR [Y/n]"
+	oldSize=$(du -csh *sql *bz2 | tail -1 | cut -f 1)
+	printf "[WARNING!]\nold backup detected\t$oldSize\nCLEAR [Y/n] : "
 	read CLEAR
 	if [[ $CLEAR = Y ]]; then
 		echo -e "CLEARING old dumps\c"
 		rm -f *.bz2 *.sql
 		echo -e "\t[DONE]"
 	else
+		echo "[EXIT]"
 		exit
 	fi
 else
-	printf "no old backup detected"
+	printf "no old backup detected\n"
 fi
-printf "Choose backup type:\n[ F ] Full backup with DB & FileS\n[ DB ] for db backup\n[ FS ] for files backup"
+printf "\n"
+echo "----------------------------------------------------------------------"
+printf "Choose backup type:\n[ F ] Full backup with DB & FileS\n[ DB ] for db backup\n[ FS ] for files backup\n Type : "
 read backupType
-	
-if [[ $backupType = @(DB | F) ]]; then
+	printf "\n"
+	echo "----------------------------------------------------------------------"
+if [[ $backupType = @(DB|F) ]] ; then
 
 USER=$(grep "^ *'username' => .*',$" $settingsDir | cut -d "'" -f 4)
 PASS=$(grep "^ *'password' => .*',$" $settingsDir | cut -d "'" -f 4)
 DB=$(grep "^ *'database' => .*',$" $settingsDir | cut -d "'" -f 4)
-	echo "----------------------------------------------------------------------"
 	echo -e "PACKING DB\c"
 
 	if mysqldump -u$USER -p$PASS $DB > dump.sql ; then
@@ -38,8 +42,8 @@ DB=$(grep "^ *'database' => .*',$" $settingsDir | cut -d "'" -f 4)
 	fi
 fi
 
-if [[ $backupType = @(FS | F) ]]; then
-	echo "----------------------------------------------------------------------"
+if [[ $backupType = @(FS|F) ]] ; then
+	printf "\n"
 	echo -e "PACKING FILES\c"
 	if tar -cjf dump.tar.bz2 * .htaccess ; then
 		echo -e "  \t\t[COMPLETE]"
@@ -48,5 +52,6 @@ if [[ $backupType = @(FS | F) ]]; then
 		exit
 	fi
 fi
-
-echo -e "go WGET it \t\t [ALL DONE]"
+printf "\n"
+echo "----------------------------------------------------------------------"
+echo -e "go WGET it \t\t[ALL DONE]"
