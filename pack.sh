7c600555aee7eb11e839468b3c3c92aacf2006ee
AtomskJD <AtomskJD@gmail.com>
2013-12-27 10:34:35 +0600

    Извлечение параметров из файла настроек
    Добавлен выбор типа дампа и проверка удаления старых дампов

--- pack.sh
+++ pack.sh
@@ -1,20 +1,32 @@
 #!/bin/bash
 # @author 	AtomskJD aka mintru9
-# version	0.10
+# version	0.12
 
 settingsDir=sites/default/settings.php
+spaceUsed=$(du -sh | cut -f 1)
+
+printf "PRE PACKING INFO\nTotal site disk usage\t$spaceUsed\n"
+if (( $( ls -l | grep 'bz2$\|sql$' | wc -l) )) ; then
+	printf "[WARNING!] old backup detected\nCLEAR [Y/n]"
+	read CLEAR
+	if [[ $CLEAR = Y ]]; then
+		echo -e "CLEARING old dumps\c"
+		rm -f *.bz2 *.sql
+		echo -e "\t[DONE]"
+	else
+		exit
+	fi
+else
+	printf "no old backup detected"
+fi
+printf "Choose backup type:\n[ F ] Full backup with DB & FileS\n[ DB ] for db backup\n[ FS ] for files backup"
+read backupType
+	
+if [[ $backupType = @(DB | F) ]]; then
 
 USER=$(grep "^ *'username' => .*',$" $settingsDir | cut -d "'" -f 4)
 PASS=$(grep "^ *'password' => .*',$" $settingsDir | cut -d "'" -f 4)
 DB=$(grep "^ *'database' => .*',$" $settingsDir | cut -d "'" -f 4)
-
-printf "Choose backup type:\n[ F ] Full backup with DB & FileS\n[ DB ] for db backup\n[ FS ] for files backup"
-read backupType
-
-rm dump.sql dump.tar.bz2
-echo "CLEARING old dumps"
-	
-if [[ $backupType = @(DB | F) ]]; then
 	echo "----------------------------------------------------------------------"
 	echo -e "PACKING DB\c"
 
