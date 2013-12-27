0372db9029c25d01e1e41af2e09725f9b9118a46
AtomskJD <AtomskJD@gmail.com>
2013-12-27 17:11:14 +0600

    Фикс для мультисайтов
    Если папка не дефолт указать первым параметром

--- pack.sh
+++ pack.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 # @author 	AtomskJD aka mintru9
-# @version	0.16
+# @version	0.17
 
-settingsDir=sites/default/settings.php
+if [ $1 ] ; then
+	settingsDir=sites/$1/settings.php
+else
+	settingsDir=sites/default/settings.php
+fi
+
 spaceUsed=$(du -sh | cut -f 1)
 echo "----------------------------------------------------------------------"
-printf "PRE PACKING INFO\nTotal site disk usage\t$spaceUsed\n"
+printf "PRE PACKING INFO : \nTotal site disk usage\t$spaceUsed\nSettings path\t\t$settingsDir\n"
 if (( $( ls -l | grep 'bz2$\|sql$' | wc -l) )) ; then
 	oldSize=$(du -csh *sql *bz2 | tail -1 | cut -f 1)
 	printf "[WARNING!]\nold backup detected\t$oldSize\nCLEAR [Y/n] : "
@@ -21,6 +26,7 @@ if (( $( ls -l | grep 'bz2$\|sql$' | wc -l) )) ; then
 else
 	printf "no old backup detected\n"
 fi
+
 printf "\n"
 echo "----------------------------------------------------------------------"
 printf "Choose backup type:\n[ F ] Full backup with DB & FileS\n[ DB ] for db backup\n[ FS ] for files backup\n[] Default Full backup\nType : "
@@ -29,18 +35,19 @@ read backupType
 	echo "----------------------------------------------------------------------"
 if [[ $backupType = @(DB|F|"") ]] ; then
 
-if (( $(sed -n "/^ *'database' => '.*'/p" sites/default/settings.php | wc -c) )) ; then
-	echo "is drupal 7"
-	USER=$(grep "^ *'username' => .*',$" $settingsDir | cut -d "'" -f 4)
-	PASS=$(grep "^ *'password' => .*',$" $settingsDir | cut -d "'" -f 4)
-	DB=$(grep "^ *'database' => .*',$" $settingsDir | cut -d "'" -f 4)
-fi
-if (( $(sed -n "/^\$db_url = 'mysqli:.*';/p" sites/default/settings.php | wc -c) )) ; then
-	echo "is drupal 7"
-	USER=$(grep "^\$db_url = 'mysqli:.*';" sites/default/settings.php | cut -c 21- | sed -e "s/@localhost\//:/" -e "s/';//" | cut -d ":" -f 1)
-	PASS=$(grep "^\$db_url = 'mysqli:.*';" sites/default/settings.php | cut -c 21- | sed -e "s/@localhost\//:/" -e "s/';//" | cut -d ":" -f 2)
-	DB=$(grep "^\$db_url = 'mysqli:.*';" sites/default/settings.php | cut -c 21- | sed -e "s/@localhost\//:/" -e "s/';//" | cut -d ":" -f 3)
-fi
+	if (( $(sed -n "/^ *'database' => '.*'/p" $settingsDir | wc -c) )) ; then
+		echo "is drupal 7"
+		USER=$(grep "^ *'username' => .*',$" $settingsDir | cut -d "'" -f 4)
+		PASS=$(grep "^ *'password' => .*',$" $settingsDir | cut -d "'" -f 4)
+		DB=$(grep "^ *'database' => .*',$" $settingsDir | cut -d "'" -f 4)
+	fi
+	if (( $(sed -n "/^\$db_url = 'mysqli:.*';/p" $settingsDir | wc -c) )) ; then
+		echo "is drupal 6"
+		USER=$(grep "^\$db_url = 'mysqli:.*';" $settingsDir | cut -c 21- | sed -e "s/@localhost\//:/" -e "s/';//" | cut -d ":" -f 1)
+		PASS=$(grep "^\$db_url = 'mysqli:.*';" $settingsDir | cut -c 21- | sed -e "s/@localhost\//:/" -e "s/';//" | cut -d ":" -f 2)
+		DB=$(grep "^\$db_url = 'mysqli:.*';" $settingsDir | cut -c 21- | sed -e "s/@localhost\//:/" -e "s/';//" | cut -d ":" -f 3)
+	fi
+
 	echo -e "PACKING DB\c"
 
 	if mysqldump -u"$USER" -p"$PASS" "$DB" > dump.sql ; then
