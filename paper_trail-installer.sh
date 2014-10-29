a4e2c7b97703a07412bdb76820c82b14414c6fe8
AtomskJD <AtomskJD@gmail.com>
2014-10-29 17:55:22 +0500

    logger installation and setup fixes

--- paper_trail-installer.sh
+++ paper_trail-installer.sh
@@ -3,21 +3,32 @@
 # @version	0.1
 ## LOG pusher INSTALLER
 
+dates=$(date)
+
 mkdir -p bin install drush
 
 wget -O $HOME/install/remote_syslog.tar.gz "https://github.com/papertrail/remote_syslog2/releases/download/v0.13/remote_syslog_linux_386.tar.gz"
 tar xzf $HOME/install/remote_syslog*.tar.gz -C $HOME/install
 cp $HOME/install/remote_syslog/* $HOME/bin
 
-rm $HOME/install/remote_syslog*
+rm -r $HOME/install/remote_syslog*
 
+# SET loggs
 echo "mail.add_x_header = On" >> $HOME/php-bin/php.ini
 echo "mail.log = $HOME/logs/mail.log" >> $HOME/php-bin/php.ini
+
+touch $HOME/logs/self.log
+touch $HOME/logs/mail.log
 
 #cp $HOME/bin/example_config.yml $HOME/bin/remote_syslog_conf.yml
 # set YOUR instance values
 
-sed -e "s/port:.*$/port: 28248/" -e "s/host:.*$/host: logs2.papertrailapp.com/" -e "s/^\s*- locallog.txt/  - \$HOME\/logs\/*.error.log\n  - \$HOME\/logs\/mail.log/" $HOME/bin/example_config.yml > $HOME/bin/remote_syslog_conf.yml
 
+
+sed -e "s/port:.*$/port: 28248/" -e "s/host:.*$/host: logs2.papertrailapp.com/" -e "s/^\s*- locallog.txt/  - logs\/*.error.log\n  - logs\/mail.log\n  - logs\/self.log/" $HOME/bin/example_config.yml > $HOME/bin/remote_syslog_conf.yml
+
+
+
+echo "$dates Successful install PAPER TRAIL" >> $HOME/logs/self.log
 
 echo -e '\033[1;32mInstallation Successful\033[0m'
---
+++ paper_trail.init.d
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+### BEGIN INIT INFO
+# Provides: remote_syslog
+# Required-Start: $network $remote_fs $syslog
+# Required-Stop: $network $remote_fs $syslog
+# Default-Start: 2 3 4 5
+# Default-Stop: 0 1 6
+# Short-Description: Start and Stop
+# Description: Runs remote_syslog
+### END INIT INFO
+
+#       /etc/init.d/remote_syslog
+#
+# Starts the remote_syslog daemon
+#
+# chkconfig: 345 90 5
+# description: Runs remote_syslog
+#
+# processname: remote_syslog
+
+prog="remote_syslog"
+config="$HOME/bin/remote_syslog_conf.yml"
+
+dates=$(date)
+
+EXTRAOPTIONS=""
+
+RETVAL=0
+
+start(){
+    echo "$dates : paper trail daemon starting" >> $HOME/logs/self.log
+
+    $prog -c $config $EXTRAOPTIONS
+
+    return 0
+}
+
+stop(){
+    echo "Stopping $prog: "
+    tmp=$(ps -A | grep remote_syslog | wc -l)
+    if [[ $tmp > 0 ]]; then
+      echo "found - killed"
+      kill $(ps -A | grep remote_syslog | awk '{print $1}')
+    else
+      echo "not found"
+    fi
+
+
+      return 0
+}
+
+status(){
+    echo -n $"Checking for process "
+
+    if ($(ps -A | grep remote_syslog | awk '{print $1}')); then
+      echo -e '\033[1;32m[ FOUND ]\033[0m'
+    else
+      echo "[ not found ]"
+    fi
+    return 0
+}
+
+
+# See how we were called.
+case "$1" in
+    start)
+	start
+	;;
+    stop)
+	stop
+	;;
+    status)
+	status
+	;;
+    *)
+	echo $"Usage: $0 {start|stop|status}"
+	RETVAL=1
+esac
+
+exit $RETVAL
